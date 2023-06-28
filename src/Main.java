c3e706420cd8d6cb008a4bb3f19cd1dfe9d1cb71
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-28 16:01:29 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // User inputs 6 different figures between 1 and 42
+        // Figures get put in an array
+        while (true) {
+
+        }
+
+        // Computer inputs 6 different figures between 1 and 42 in an array
+        for (int i = 0; i < 6; i++){
+
+        }
+    }
+}
