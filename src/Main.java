97768422cfa77890a7b6d732476e296800564c74
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-28 16:53:34 +0200

    UserInput aangemaakt en in array geplaatst

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import javax.sound.midi.Soundbank;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList userInput = new ArrayList();
 
-        // User inputs 6 different figures between 1 and 42
-        // Figures get put in an array
-        while (true) {
+        //For loop gebruiken i.p.v. while
+        //Ongewenste waarde is i--
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Geef een getal tussen de 1 en 42.");
+            int input = scanner.nextInt();
 
+            if (input >= 1 && input <= 42) {
+                userInput.add(input);
+                System.out.println(userInput);
+            } else {
+                i--;
+                System.out.println("Het getal moet tussen de 1 en 42 zijn.");
+            }
         }
+
+        System.out.println(userInput);
 
         // Computer inputs 6 different figures between 1 and 42 in an array
         for (int i = 0; i < 6; i++){
