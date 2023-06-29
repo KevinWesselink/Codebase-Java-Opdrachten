3bdb3d10fccffa2b96616e3fa5281365ef2e603a
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-29 10:41:43 +0200

    UserInput sort added

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,36 @@
-import javax.sound.midi.Soundbank;
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList userInput = new ArrayList();
-        ArrayList computerInput = new ArrayList();
+        int[] userInput = new int[6];
+        int[] computerInput = new int[6];
 
-        //For loop gebruiken i.p.v. while
-        //Ongewenste waarde is i--
         for (int i = 0; i < 6; i++) {
             System.out.print("Geef een getal tussen de 1 en 42. ");
             int input = scanner.nextInt();
 
             if (input >= 1 && input <= 42) {
-                userInput.add(input);
-                System.out.println(userInput);
+                userInput[i] = input;
+                System.out.println(Arrays.toString(userInput));
             } else {
                 i--;
                 System.out.println("Het getal moet tussen de 1 en 42 zijn. ");
             }
         }
 
-        // Computer inputs 6 different figures between 1 and 42 in an array
+        Arrays.sort(userInput);
+        System.out.println(Arrays.toString(userInput));
+
         for (int i = 0; i < 6; i++){
             int random = ThreadLocalRandom.current().nextInt(1, 42);
-            computerInput.add(random);
-            System.out.println(computerInput);
+            computerInput[i] = random;
         }
+
+//        Arrays.sort(computerInput);
+        System.out.println(Arrays.toString(computerInput));
     }
 }
