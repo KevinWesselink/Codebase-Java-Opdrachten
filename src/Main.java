0c3b6d1da3bec0394aad53706f34f0d932190803
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-29 08:54:06 +0200

    Fix computerInput and put into array

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import javax.sound.midi.Soundbank;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList userInput = new ArrayList();
+        ArrayList computerInput = new ArrayList();
 
         //For loop gebruiken i.p.v. while
         //Ongewenste waarde is i--
         for (int i = 0; i < 6; i++) {
-            System.out.print("Geef een getal tussen de 1 en 42.");
+            System.out.print("Geef een getal tussen de 1 en 42. ");
             int input = scanner.nextInt();
 
             if (input >= 1 && input <= 42) {
@@ -19,15 +21,16 @@ public class Main {
                 System.out.println(userInput);
             } else {
                 i--;
-                System.out.println("Het getal moet tussen de 1 en 42 zijn.");
+                System.out.println("Het getal moet tussen de 1 en 42 zijn. ");
             }
         }
 
-        System.out.println(userInput);
-
         // Computer inputs 6 different figures between 1 and 42 in an array
         for (int i = 0; i < 6; i++){
-
+            int random = ThreadLocalRandom.current().nextInt(1, 42);
+            System.out.println(random);
+            computerInput.add(random);
+            System.out.println(computerInput);
         }
     }
 }
