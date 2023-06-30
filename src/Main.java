27c1512a54c289c5536be78bac068e308884eb41
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-30 10:04:22 +0200

    Looping through array now works

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int[] userInput = new int[6];
         int[] computerInput = new int[6];
+        int validation = 0;
 
         for (int i = 0; i < 6; i++) {
             System.out.print("Geef een getal tussen de 1 en 42. ");
@@ -20,11 +21,10 @@ public class Main {
                 }
             }
 
-            System.out.println(found);
+//            System.out.println(found);
 
             if (input >= 1 && input <= 42 && !found) {
                 userInput[i] = input;
-                System.out.println(Arrays.toString(userInput));
             } else {
                 i--;
                 System.out.println("Het getal moet tussen de 1 en 42 zijn. ");
@@ -54,5 +54,36 @@ public class Main {
 
         Arrays.sort(computerInput);
         System.out.println(Arrays.toString(computerInput));
+
+        for (int i = 0; i < 6; i++) {
+            int userNumber = userInput[i];
+
+            for (int j = 0; j < 6; j++) {
+                int computerNumber = computerInput[j];
+
+                if (userNumber == computerNumber) {
+                    validation++;
+                }
+            }
+        }
+
+        System.out.println("Aantal punten: " + validation);
+
+        switch (validation) {
+            case 3:
+                System.out.println("Je wint 10 euro!");
+                break;
+            case 4:
+                System.out.println("Je wint 1.000 euro!");
+                break;
+            case 5:
+                System.out.println("Je wint 100.000 euro!");
+                break;
+            case 6:
+                System.out.println("Je wint 10.000.000 euro!");
+                break;
+            default:
+                System.out.println("Je wint helemaal niets!");
+        }
     }
 }
